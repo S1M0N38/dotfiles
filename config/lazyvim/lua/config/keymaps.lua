afed49107baf89cb14fc40eab1be9cd95d549235
S1M0N38 <bertolottosimone@gmail.com>
2024-02-04 16:56:53 +0100

    feat(lazyvim): add personal keymaps
    - Improved paste/yank
    - Better "J"
    - Toggle linebreak

--- config/lazyvim/lua/config/keymaps.lua
+++ config/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local map = vim.keymap.set
+
+-- Improved yank/paste/delete
+map("x", "<leader>p", '"_dP', { desc = "Paste without overwriting the default register" })
+map({ "n", "v" }, "<leader>d", '"_d', { desc = "Delete without overwriting the default register" })
+map({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
+map({ "n", "v" }, "<leader>Y", '"+Y', { desc = "Yank to system clipboard" })
+
+-- better "J" (keep cursor in place)
+map("n", "J", "mzJ`z")
+
+-- Toggle linebreak
+map("n", "<leader>uW", "<cmd>set linebreak!<cr>", { desc = "Toggle Line Break" })
