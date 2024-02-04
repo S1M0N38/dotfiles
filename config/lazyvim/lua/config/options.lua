d0027f1d53c9a0b03242476739ae18b831d0c86f
S1M0N38 <bertolottosimone@gmail.com>
2024-02-04 17:33:49 +0100

    feat(lazyvim): add personal options
    - Disable system clipboard as main clipboard
    - Disable provider
    - Enable python provieder
    - Set languages for spelllang
    - VimTex global vars

--- config/lazyvim/lua/config/options.lua
+++ config/lazyvim/lua/config/options.lua
@@ -1,3 +1,24 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Disable copy to system clipboard. See keymaps
+vim.opt.clipboard = ""
+
+-- Disable providers
+vim.g.loaded_python_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+
+-- Enable providers
+-- I've have created a virtual environment for neovim.
+-- Using a python version installed with pyenv, I've created a virtual environment
+-- with the following command:
+-- .local/share/pyenv/versions/3.10.13/bin/python -m venv .local/share/venv/neovim
+-- Then installed the neovim package with the corresponding pip:
+-- ~/.local/share/venv/neovim/bin/python -m pip install pynvim
+vim.g.python3_host_prog = "~/.local/share/venv/neovim/bin/python"
+
+-- Language
+vim.opt.spelllang = { "en", "it" }
+
+-- VimTex
+vim.g.tex_flavor = "latex"
+vim.g.vimtex_compiler_latexmk = { aux_dir = "aux" }
