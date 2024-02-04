1a7e7150b9163817490a91f0a0fe32f2d690cb4e
S1M0N38 <bertolottosimone@gmail.com>
2024-02-04 16:52:09 +0100

    feat(lazyvim): disable plugins

---
+++ config/lazyvim/lua/plugins/disable.lua
@@ -0,0 +1,7 @@
+return {
+  { "folke/flash.nvim", enabled = false },
+  { "mfussenegger/nvim-lint", enabled = false },
+  { "lukas-reineke/headlines.nvim", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
+}
