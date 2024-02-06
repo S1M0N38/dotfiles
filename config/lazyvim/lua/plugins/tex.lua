3d0d10721eb2d072015ca9e812b721e4d0ce3333
S1M0N38 <bertolottosimone@gmail.com>
2024-02-06 18:54:18 +0100

    feat(lazyvim): add config for tex lang

---
+++ config/lazyvim/lua/plugins/tex.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        texlab = {
+          settings = {
+            texlab = {
+              diagnostics = {
+                ignoredPatterns = { "Unused label", "Unused entry" },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
