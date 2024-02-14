abe8938edbf35aaf1a173e55dca947edff202a5d
S1M0N38 <bertolottosimone@gmail.com>
2024-02-14 21:37:24 +0100

    fix: add TS parser for nvim dev

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,6 @@
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
+require("nvim-treesitter.configs").setup({
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+})
