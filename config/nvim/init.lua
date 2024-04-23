7c82b923c170c33493f5d1c8209db27a9738b64e
S1M0N38 <bertolottosimone@gmail.com>
2024-04-23 13:49:30 +0200

    fix: treesitter error in brew install --HEAD

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,8 @@
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
-require("nvim-treesitter.configs").setup({
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-})
+
+-- `brew install neovim --HEAD`
+-- does not provide treesitter parser, so you have to manually install them
+-- Install `nvim-treesitter` plugin in the manual way by following:
+-- https://www.perplexity.ai/search/how-to-install-meW1.yIpQxSEFEjdz3RsCQ
