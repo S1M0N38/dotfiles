30e2162ab12323f59840716d605c37c339d43df2
S1M0N38 <bertolottosimone@gmail.com>
2024-05-26 14:53:11 +0200

    feat(lazyvim): manually configure python
    - use ruff for linting, formatting and import sorting
    - disable pyright dignostic
    - use jupynium.nvim for interact with classic jupyter notebook

--- config/lazyvim/lua/plugins/python.lua
+++ config/lazyvim/lua/plugins/python.lua
@@ -1,14 +1,147 @@
 return {
+  recommended = function()
+    return LazyVim.extras.wants({
+      ft = "python",
+      root = {
+        "pyproject.toml",
+        "setup.py",
+        "setup.cfg",
+        "requirements.txt",
+        "Pipfile",
+        "pyrightconfig.json",
+      },
+    })
+  end,
+
+  -- Treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "ninja", "python", "rst", "toml" })
+      end
+    end,
+  },
+
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+
+        -- Use ruff for linting, formatting, and organizing imports
+        -- Use pyright for textDocument/hover
         pyright = {
-          diagnosticMode = "openFilesOnly",
-          useLibraryCodeForTypes = false,
-          autoSearchPaths = false,
+          settings = {
+            pyright = {
+              disableOrganizeImports = true,
+            },
+            python = {
+              analysis = {
+                ignore = { "*" },
+              },
+            },
+          },
+        },
+        ruff = {
+          enabled = true,
+          keys = {
+            {
+              "<leader>co",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.organizeImports" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Organize Imports",
+            },
+          },
         },
       },
+      setup = {
+        ruff = function()
+          LazyVim.lsp.on_attach(function(client, _)
+            if client.name == "ruff" then
+              -- Disable hover in favor of Pyright
+              client.server_capabilities.hoverProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
+
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "nvim-neotest/neotest-python",
+    },
+    opts = {
+      adapters = {
+        ["neotest-python"] = {
+          runner = "pytest",
+          python = ".venv/bin/python",
+        },
+      },
+    },
+  },
+
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      "mfussenegger/nvim-dap-python",
+      -- stylua: ignore
+      keys = {
+        { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
+        { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
+      },
+      config = function()
+        local path = require("mason-registry").get_package("debugpy"):get_install_path()
+        require("dap-python").setup(path .. "/venv/bin/python")
+      end,
+    },
+  },
+
+  {
+    "linux-cultist/venv-selector.nvim",
+    cmd = "VenvSelect",
+    opts = function(_, opts)
+      if LazyVim.has("nvim-dap-python") then
+        opts.dap_enabled = true
+      end
+      return vim.tbl_deep_extend("force", opts, {
+        name = {
+          "venv",
+          ".venv",
+          "env",
+          ".env",
+        },
+      })
+    end,
+    keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv", ft = "python" } },
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      opts.auto_brackets = opts.auto_brackets or {}
+      table.insert(opts.auto_brackets, "python")
+    end,
+  },
+
+  {
+    "kiyoon/jupynium.nvim",
+    build = vim.g.loaded_python_provider .. " -m pip install .",
+    lazy = false,
+    opts = {
+      python_host = vim.g.loaded_python_provider,
+      default_notebook_URL = "localhost:8888/nbclassic",
+      jupyter_command = "jupyter",
     },
   },
 }
