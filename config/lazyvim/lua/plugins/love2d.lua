a87f6af6129f690436f90d5a0b71e087efd14ed5
S1M0N38 <bertolottosimone@gmail.com>
2024-05-26 14:42:28 +0200

    feat(lazyvim): add love2d plugin (local)

---
+++ config/lazyvim/lua/plugins/love2d.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "love2d.nvim",
+    dir = "~/Developer/love2d.nvim",
+    cmd = "LoveRun",
+    opts = {
+      path_to_love_bin = "/Applications/love.app/Contents/MacOS/love",
+    },
+    keys = {
+      { "<leader>v", ft = "lua", desc = "LÖVE" },
+      { "<leader>vv", "<cmd>LoveRun<cr>", ft = "lua", desc = "Run LÖVE" },
+      { "<leader>vs", "<cmd>LoveStop<cr>", ft = "lua", desc = "Stop LÖVE" },
+    },
+  },
+}
