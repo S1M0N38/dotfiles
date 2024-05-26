e108d6b74d006c65bd5a27398518ca6a31097fee
S1M0N38 <bertolottosimone@gmail.com>
2024-05-26 14:23:03 +0200

    feat(lazyvim): setup python provider

--- config/lazyvim/lua/config/options.lua
+++ config/lazyvim/lua/config/options.lua
@@ -2,11 +2,14 @@
 vim.opt.clipboard = ""
 
 -- Disable providers
-vim.g.loaded_python_provider = 0
-vim.g.loaded_python3_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
+
+-- Python
+vim.g.python3_host_prog = vim.fn.expand("$XDG_DATA_HOME") .. "/venvs/neovim/bin/python"
+vim.g.loaded_python_provider = vim.g.python3_host_prog
+vim.g.loaded_python3_provider = vim.g.python3_host_prog
 
 -- Language
 vim.opt.spelllang = { "en", "it" }
