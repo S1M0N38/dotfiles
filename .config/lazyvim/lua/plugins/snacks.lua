b9540a518aac137bb5231bf7306179c2afcf2f1a
S1M0N38 <bertolottosimone@gmail.com>
2025-04-24 09:37:05 +0200

    fix(lazyvim): serach $XDG_CONFIG_HOME instead of lazyvim config

--- .config/lazyvim/lua/plugins/snacks.lua
+++ .config/lazyvim/lua/plugins/snacks.lua
@@ -2,8 +2,28 @@ return {
   "folke/snacks.nvim",
   opts = {
     dashboard = {
-      sections = {
-        { section = "keys", gap = 1, padding = 1 },
+      preset = {
+        pick = function(cmd, opts)
+          return LazyVim.pick(cmd, opts)()
+        end,
+        header = "",
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ':lua Snacks.dashboard.pick("files", {cwd = vim.fn.expand("$XDG_CONFIG_HOME")})',
+            -- NOTE: open XDG_CONFIG_HOME instead of Lazyvim Config
+          },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
       },
     },
     terminal = {
@@ -12,6 +32,16 @@ return {
           winbar = "",
         },
       },
+    },
+  },
+
+  keys = {
+    {
+      "<leader>fc",
+      function()
+        Snacks.picker.files({ cwd = vim.fn.expand("$XDG_CONFIG_HOME") })
+      end,
+      desc = "Find Config File",
     },
   },
 }
