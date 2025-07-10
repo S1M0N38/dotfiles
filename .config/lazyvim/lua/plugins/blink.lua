9008dd95884edae6a8b562bf56cec471ae31b104
S1M0N38 <bertolottosimone@gmail.com>
2025-07-11 00:15:39 +0200

    feat(lazyvim): add config for blink.cmp

---
+++ .config/lazyvim/lua/plugins/blink.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "default",
+      },
+      completion = { documentation = { auto_show = true } },
+      cmdline = {
+        completion = {
+          menu = { auto_show = true },
+          ghost_text = { enabled = true },
+        },
+      },
+    },
+  },
+}
